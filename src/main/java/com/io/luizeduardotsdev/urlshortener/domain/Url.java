b17d4fde487dd73c588d6f55867e3c5554fefcfe
luiz eduardo <luizeduardotsdev@gmail.com>
2026-02-24 12:57:05 -0300

    feat: add localdate na entidade e annotation collumn

--- src/main/java/com/io/luizeduardotsdev/urlshortener/domain/Url.java
+++ src/main/java/com/io/luizeduardotsdev/urlshortener/domain/Url.java
@@ -3,6 +3,8 @@ package com.io.luizeduardotsdev.urlshortener.domain;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Data
 @Table(name = "url")
@@ -12,7 +14,9 @@ public class Url {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
-    private String url;
+    @Column(nullable = false, unique = true, length = 500)
+    private String originalUrl;
     private String shortUrl;
+
+    private LocalDateTime createdAt;
 }
